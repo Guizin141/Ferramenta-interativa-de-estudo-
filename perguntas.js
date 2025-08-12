1b706ddd35aa3ca77d1e33b53b49344c459634b3
Guizin141 <guilherme.palomo@escola.pr.gov.br>
2025-08-12 12:14:53 -0300

    ////

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+    'Futebol',
+    Qual o clube com mais títulos oficiais no mundo?',
+    'Real Madrid'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual a seleção com mais medalhas olímpicas?',
+    'Estados Unidos com 2636 medalhas'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual o técnico com mais títulos oficiais?',
+    'Sir Alex Ferguson com 49 títulos'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual o jogador com mais títulos oficiais?',
+    'Lionel Messi'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual o clube brasileiro mais antigo?',
+    'Sport Club Rio Grande '
+)
+
+criaCartao(
+    'Futebol',
+    'Qual o jogador com mais gols oficiais?',
+    'Cristiano Ronaldo. No dia 12/08/2025 ele está com 938 gols'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual jogador com mais gols de falta?',
+    'Marcelinho Carioca com 86 gols'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual jogador com mais gols de pênaltis?',
+    'Lionel Messi com 108 gols'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual o zagueiro com mais gols?',
+    'Ronald Koeman com 253 gols'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual o chute mais forte da história?',
+    'Ronny Heberson sendo 221 km/h '
+)
+
+criaCartao(
+    'Futebol',
+    'Qual o jogador que atingiu a maior velocidade?',
+    'Gabriel Silva com 40,3 km/h'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual o jogador com mai faltas sofridas?',
+    'Neymar Jr com 893 faltas sofridas com uma média de 4,06 por jogo'
+)
